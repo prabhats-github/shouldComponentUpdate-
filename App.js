f6f3b6567182108f0bab112b20b936d0ed68f0ff
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-02-16 15:00:58 +0530

    Add files via upload
    Practicing shouldComponentUpdate(  )

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Shouldupdate from './components/Shouldupdate';
+
+function App() {
+  return (
+    <div className="App">
+      <Shouldupdate/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Shouldupdate.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+class Shouldupdate extends React.Component {
+    state = {color:"red"  } 
+
+    // shouldComponentUpdate(){
+    //     return false;
+    // }
+    change =()=>{
+        this.setState({color:"blue"})
+    }
+    render() { 
+        return (
+            <div>
+
+            <h1>My color is {this.state.color}</h1>
+            <button onClick={this.change}>Change Color</button>
+
+            </div>
+        );
+    }
+}
+ 
+export default Shouldupdate;
